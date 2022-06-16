c2264e69e961158ddab65c9e6472c7637aa2d2bb
rajeshmn47 <rajeshmn47@gmail.com>
2022-06-16 16:38:29 +0530

    make it better

---
+++ assign.js
@@ -0,0 +1,12 @@
+//Given a string, find the reverse of it.
+function reversestring(s){
+    var string=s
+    a=[]
+    for(i=string.length;i>-1;i--){
+        a.push(string[i])
+    }
+
+    console.log(a.join(''))
+}
+
+reversestring('phicode')
--- server.js
+++ server.js
@@ -1,14 +1,39 @@
-array = [2,5,6,3,4,20,12,20,4,11,5,2,3,45,51,6,25,7,11]
+array = [4,4,1,1,2,4,5,2]
 function filtering(a){
     k=[]
 for(i=0;i<a.length;i++){
   for(j=i+1;j<a.length;j++){
-if(a[i]===a[j]){
+    if(!k.includes(a[j])){
+      if(a[i]===a[j]){
     k.push(a[i])
-}
+      }
+    }
   }
     
 }
 console.log(k)
 }
 filtering(array)
+
+function filter(a){
+  k=[]
+  j={}
+for(i=0;i<a.length;i++){
+if(j[a[i]]){
+
+}
+else{
+  k.push(a[i])
+  
+}
+j[a[i]]=a[i]
+}
+console.log(k)
+}
+var string = "Welcome to this Javascript Guide!";
+function reverseBySeparator(string, separator) {
+  return string.split(separator).reverse().join(separator);
+}
+//var reverseEntireSentence = reverseBySeparator(string, " ");
+//var reverseEachWord = reverseBySeparator(reverseEntireSentence, "");
+//console.log(reverseEachWord)
